0af17620d37b9959d45a12d7f949b3beb32ccb43
jimmy319 <jimmy74319@gmail.com>
2019-04-14 18:25:21 +0800

    Adopt Webpack@4 in global scoped css example

--- examples/global-scoped-css/webpack.config.dev.js
+++ examples/global-scoped-css/webpack.config.dev.js
@@ -1,9 +1,10 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WriteFilePlugin = require('write-file-webpack-plugin')
 const AmpReactRendererPlugin = require('amp-react-renderer-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     home: [
       path.resolve(__dirname, './src/styles/global.css'),
@@ -22,24 +23,26 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              query: {
-                minimize: true
-              }
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            query: {
+              minimize: true
             }
-          ]
-        })
+          }
+        ]
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
       filename: '[name].css',
-      disable: false,
-      allChunks: false
+      chunkFilename: '[id].css',
     }),
     new AmpReactRendererPlugin(),
     new WriteFilePlugin()
